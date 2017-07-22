a086273709d2a18c21096c7979024e3fc6327436
Parth Vora <parth.v@innovify.in>
2017-07-22 17:53:30 +0530

    Correct spacing in blade views

--- resources/views/country/list.blade.php
+++ resources/views/country/list.blade.php
@@ -9,49 +9,49 @@
                 <div class="panel-body">
                     <table id="country-table" class="table table-striped">
                         <thead>
-                          <tr>
-                            <th>Name</th>
-                            <th>Capital</th>
-                            <th>Alpha2 Code</th>
-                            <th>Calling Codes</th>
-                            <th>Population</th>
-                            <th>Region</th>
-                            <th>Currencies</th>
-                        </tr>
-                    </thead>
-                    <tfoot>
-                        <tr>
-                            <th></th>
-                            <th></th>
-                            <th class="searchable"></th>
-                            <th></th>
-                            <th></th>
-                            <th></th>
-                            <th class="searchable"></th>
-                        </tr>
-                    </tfoot>
-                    <tbody>
-                        @foreach($allCountries as $country)
-                        <tr>
-                            <td><a href="{{ route('country.show', [$country->alpha2Code]) }}">{{ $country->name }}</a></td>
-                            <td>{{ $country->capital }}</td>
-                            <td>{{ $country->alpha2Code }}</td>
-                            <td>{{ implode(', ', $country->callingCodes) }}</td>
-                            <td>{{ $country->population }}</td>
-                            <td>{{ $country->region }}</td>
-                            <td>
-                                @foreach($country->currencies as $currency)
-                                <a href="{{ route('country.currency', [$currency->code]) }}">{{ $currency->code }}</a>
-                                @if(!$loop->last),@endif
-                                @endforeach
-                            </td>
-                        </tr>
-                        @endforeach
-                    </tbody>
-                </table>
+                            <tr>
+                                <th>Name</th>
+                                <th>Capital</th>
+                                <th>Alpha2 Code</th>
+                                <th>Calling Codes</th>
+                                <th>Population</th>
+                                <th>Region</th>
+                                <th>Currencies</th>
+                            </tr>
+                        </thead>
+                        <tfoot>
+                            <tr>
+                                <th></th>
+                                <th></th>
+                                <th class="searchable"></th>
+                                <th></th>
+                                <th></th>
+                                <th></th>
+                                <th class="searchable"></th>
+                            </tr>
+                        </tfoot>
+                        <tbody>
+                            @foreach($allCountries as $country)
+                            <tr>
+                                <td><a href="{{ route('country.show', [$country->alpha2Code]) }}">{{ $country->name }}</a></td>
+                                <td>{{ $country->capital }}</td>
+                                <td>{{ $country->alpha2Code }}</td>
+                                <td>{{ implode(', ', $country->callingCodes) }}</td>
+                                <td>{{ $country->population }}</td>
+                                <td>{{ $country->region }}</td>
+                                <td>
+                                    @foreach($country->currencies as $currency)
+                                    <a href="{{ route('country.currency', [$currency->code]) }}">{{ $currency->code }}</a>
+                                    @if(!$loop->last),@endif
+                                    @endforeach
+                                </td>
+                            </tr>
+                            @endforeach
+                        </tbody>
+                    </table>
+                </div>
             </div>
         </div>
     </div>
-</div>
 </div>
 @endsection
